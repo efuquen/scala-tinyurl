be2ffd0ac1291497280f22f4445889e808efa095
Edwin Fuquen <efuquen@gmail.com>
2012-06-28 16:22:33 -0400

    misnamed the servlet in web.xml.  container starting works now

--- src/main/scala/com/livestream/TinyURLServlet.scala
+++ src/main/scala/com/livestream/TinyURLServlet.scala
@@ -2,16 +2,16 @@ package com.livestream.tinyurl
 
 import org.scalatra._
 
-import com.weiglewilczek.slf4s._
+//import com.weiglewilczek.slf4s._
 
-class TinyURLServlet extends ScalatraServlet with Logging {
+class TinyURLServlet extends ScalatraServlet {
   get("/") {
     params.get("url") match {
       case Some(url) =>
-        logger.info("URL: %s".format(url))
+        println("URL: %s".format(url))
         url
       case None =>
-        logger.warn("NoParamUrl")
+        println("NoParamUrl")
         "NoParam"
     }
   }
